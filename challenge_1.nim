c849baade45c7454a73c684c2632009361541953
Xabi Bello <xbello@gmail.com>
2017-06-09 14:26:23 +0200

    Added the operation executor.

--- challenge_1.nim
+++ challenge_1.nim
@@ -12,8 +12,28 @@ import strutils
 
 echo "Input six integers and the target: "
 var input: seq[string] = split(readLine(stdin))
-assert len(input) == 7
+assert input.len == 7
 
 var numbers: seq[int] = @[]
 for number in input:
   numbers.add(parse_int(number))
+
+var user_nums: seq[int] = numbers[0..^2]
+var target: int = numbers[numbers.len - 1]
+
+proc A(a, b: int): int =
+  a + b
+proc S(a, b: int): int =
+  a - b
+proc M(a, b: int): int =
+  a * b
+proc D(a, b: int): int =
+  a div b
+
+proc execute(operands: seq[int], operation: proc): int =
+  operation(operands[0], operands[1])
+
+var ops: seq[proc] = @[A, S, M, D]
+
+for op in ops:
+    echo execute(user_nums[0..2], op)
