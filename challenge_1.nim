2d21c9849fa2f6da336d8a933561f9f8de529101
Xabi Bello <xbello@gmail.com>
2017-06-09 13:15:51 +0200

    Initialized the practices.

---
+++ challenge_1.nim
@@ -0,0 +1,9 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/6fe9cv/20170605_challenge_318_easy_countdown_game_show/
+
+# The user should input any 6 numbers (integers) and the target number
+# The program outputs the operations to get the target
+# 25 100 9 7 3 7 881
+# Outputs 7 * 3 + 100 * 7 + 25 + 9 = 881
+# 6 75 3 25 50 100 952
+# Outputs
+# 100 + 6 * 3 * 75 - 50 / 25 = 952
